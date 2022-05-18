50717d791734214f1c17fde102abcbe2ce633821
dvicofirn <dvirchn333@gmail.com>
2022-05-18 10:27:30 +0300

    -v 0.2.3 Update Override in 2 arithmatic classes

--- src/MultiAddition.java
+++ src/MultiAddition.java
@@ -1,2 +1,18 @@
 public class MultiAddition extends Expression {
+    private Expression expression1;
+    private Expression expression2;
+    public MultiAddition(Expression expression1, Expression expression2){
+        this.expression1=expression1;
+        this.expression2=expression2;
+    }
+
+    @Override
+    public double evaluate() {
+        return this.expression1.evaluate()*this.expression2.evaluate();
+    }
+
+    @Override
+    public String toString() {
+        return "("+this.expression1.toString()+" * "+this.expression2.toString()+")";
+    }
 }
