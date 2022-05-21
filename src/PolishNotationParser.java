c1df2935bf362687810199750f8fffdce150e5f2
dvicofirn <dvirchn333@gmail.com>
2022-05-21 18:44:40 +0300

    -v 0.3.0 Implementation of parse

--- src/PolishNotationParser.java
+++ src/PolishNotationParser.java
@@ -1,9 +1,47 @@
 public class PolishNotationParser extends ExpressionParser {
-
-
+    public PolishNotationParser(){
+    }
 
     @Override
     public Expression parse(String expressionString) {
-        return null;
+        Stack<Expression> expressionStack = new Stack<>();
+        String[] expressionArr = expressionString.split(" ");
+
+        for(int i=expressionArr.length-1; i>=0; i--){
+            int typeOfString =typeOfString(expressionArr[i]);
+            switch (typeOfString){
+                case 0:
+                    Expression expression1, expression2;
+                    expression1=expressionStack.pop();
+                    expression2=expressionStack.pop();
+                    switch (expressionArr[i].charAt(0)){
+                        case '+':
+                            expressionStack.push(new Addition(expression1,expression2));
+                            break;
+                        case '-':
+                            expressionStack.push(new Subtraction(expression1,expression2));
+                            break;
+                        case '*':
+                            expressionStack.push(new Multiplication(expression1,expression2));
+                            break;
+                        case '/':
+                            expressionStack.push(new Division(expression1,expression2));
+                            break;
+                    }
+                    break;
+                case 1:
+                    double doubleNumber = Double.parseDouble(expressionArr[i]);
+                    expressionStack.push(new DoubleLiteral(doubleNumber));
+                    break;
+                case 2:
+                    double intNumber = Double.parseDouble(expressionArr[i]);
+                    expressionStack.push(new IntegerLiteral(intNumber));
+                case 3:
+                    Expression unaryMinus = new UnaryMinus(expressionStack.pop());
+                    expressionStack.push(unaryMinus);
+                    break;
+            }
+        }
+        return expressionStack.pop();
     }
 }
