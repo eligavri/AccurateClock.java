ac8c47b51d6720e42d9a1dd52baa83f1360784c5
eligavri <eliezer.gav@gmail.com>
2022-05-22 10:44:32 +0300

    final_version 1.0

--- src/Addition.java
+++ src/Addition.java
@@ -1,16 +1,33 @@
+/**
+ * Defining a class representing an addition operation.
+ * extends expression class.
+ */
 public class Addition extends Expression{
+    /**
+     * Defining parameters.
+     */
     private Expression expression1;
     private Expression expression2;
+    /**
+     * defining a constructor.
+     * @param expression1 first expression
+     * @param expression2 second expression.
+     */
     public Addition(Expression expression1, Expression expression2){
         this.expression1=expression1;
         this.expression2=expression2;
     }
-
+    /**
+     * calculates the arithmetic expression value, by adding expressions values.
+     * @return the calculated value of the expressions.
+     */
     @Override
     public double evaluate() {
         return this.expression1.evaluate()+this.expression2.evaluate();
     }
-
+    /**
+     * @return the representation as an arithmetic addition expression.
+     */
     @Override
     public String toString() {
         return "("+this.expression1.toString()+" + "+this.expression2.toString()+")";
--- src/DoubleLiteral.java
+++ src/DoubleLiteral.java
@@ -1,15 +1,29 @@
+/**
+ * Defining a class representing a double type number .
+ * extends expression class.
+ */
 public class DoubleLiteral extends Expression{
+    /**
+     * Defining parameters.
+     */
     private double literal;
-
+    /**
+     * defining a constructor.
+     * @param literal double type number.
+     */
     public DoubleLiteral(double literal){
         this.literal=literal;
     }
-
+    /**
+     * @return the double type number value.
+     */
     @Override
     public double evaluate() {
         return literal;
     }
-
+    /**
+     * @return the representation as an arithmetic double type number.
+     */
     @Override
     public String toString() {
         return "("+ this.literal+")";
